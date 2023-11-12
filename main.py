28ca804cd999ec7668fa05cf786358948b56987c
Oscar_AGS <122835599+osaradahir@users.noreply.github.com>
2023-11-12 14:25:46 -0600

    Update main.py

--- main.py
+++ main.py
@@ -27,14 +27,61 @@ class Contacto(BaseModel):
     nombre : str
     telefono : str
 
+@app.post("/contactos")
+async def crear_contacto(contacto: Contacto):
+    c = conn.cursor()
+    c.execute('INSERT INTO contactos (email, nombre, telefono) VALUES (?, ?, ?)',
+              (contacto.email, contacto.nombre, contacto.telefono))
+    conn.commit()
+    return contacto
+
 @app.get("/contactos")
 async def obtener_contactos():
     """Obtiene todos los contactos."""
-    # DONE Consulta todos los contactos de la base de datos y los envia en un JSON
     c = conn.cursor()
-    c.execute('SELECT * FROM contactos')
+    c.execute('SELECT * FROM contactos;')
     response = []
     for row in c:
-        contacto = {"email":row[0], "nombre":row[1], "telefono":row[2]}
+        contacto = {"email":row[0],"nombre":row[1], "telefono":row[2]}
         response.append(contacto)
     return response
+
+
+@app.get("/contactos/{email}")
+async def obtener_contacto(email: str):
+    """Obtiene un contacto por su email."""
+    # Consulta el contacto por su email
+    c = conn.cursor()
+    c.execute('SELECT * FROM contactos WHERE email = ?', (email,))
+    contacto = None
+    for row in c:
+        contacto = {"email":row[0],"nombre":row[1],"telefono":row[2]}
+    return contacto
+
+
+
+@app.put("/contactos/{email}")
+async def actualizar_contacto(email: str, nuevo_contacto: Contacto):
+    # Verifica si el nuevo email ya existe en la base de datos
+    c = conn.cursor()
+    c.execute('SELECT * FROM contactos WHERE email = ?', (nuevo_contacto.email,))
+    existing_contact = c.fetchone()
+
+    if existing_contact:
+        return {"error": "El nuevo correo electrónico ya está en uso.", "status_code": 400}
+
+    # Actualiza el contacto
+    c.execute('UPDATE contactos SET email = ?, nombre = ?, telefono = ? WHERE email = ?',
+              (nuevo_contacto.email, nuevo_contacto.nombre, nuevo_contacto.telefono, email))
+    conn.commit()
+    
+    return nuevo_contacto
+
+@app.delete("/contactos/{email}")
+async def eliminar_contacto(email: str):
+    """Elimina un contacto."""
+    # TODO Elimina el contacto de la base de datos
+    c = conn.cursor()
+    c.execute('DELETE FROM contactos WHERE email = ?', (email,))
+    conn.commit()
+    return {"mensaje":"Contacto eliminado"}
